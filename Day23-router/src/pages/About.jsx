38149396ee939efed92426a7b1ea76175c8745d8
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-04-24 17:23:49 +0545

    practicing useState hooks

--- Day23-router/src/pages/About.jsx
+++ Day23-router/src/pages/About.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function About() {
+  const [accordion, setAccordion] = useState([
+    {
+      id: 1,
+      title: "Accordion 1",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla,itaque!",
+      active: true,
+    },
+    {
+      id: 2,
+      title: "Accordion 2",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla,itaque!",
+      active: false,
+    },
+    {
+      id: 3,
+      title: "Accordion 3",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla,itaque!",
+      active: false,
+    },
+  ]);
+  const ItemClicked = (item) => {
+    setAccordion(
+      accordion.map((element) => {
+        if (element.id === item.id) element.active = true;
+        else element.active = false;
+        return element;
+      })
+    );
+  };
   return (
-    <div>About</div>
-  )
+    <>
+      <h1>About</h1>
+      <ul className="accr">
+        {accordion.map((item) => {
+          return (
+            <li key={item.id}>
+              <div className="title" onClick={() => ItemClicked(item)}>
+                {item.title}
+              </div>
+              {item.active && <div className="content">{item.content}</div>}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
-export default About
+export default About;
--- Day23-router/src/pages/Home.jsx
+++ Day23-router/src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useState } from "react";
 
 function Home() {
+  let [Counter, setCounter] = useState(10); //[statevalue , stateupdatefunction()]
+
+  const inc = () => {
+    setCounter((prevCounter) => {
+      return prevCounter + 1;
+    });
+  };
+  const dec = () => {
+    Counter -= 1;
+    setCounter(Counter);
+  };
   return (
-    <div>Home</div>
-  )
+    <div>
+      <button onClick={dec}>-</button>
+      <span styl={{ fontSize: "32pt", padding: "0 25px" }}>{Counter}</span>
+      <button onClick={inc}>+</button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
